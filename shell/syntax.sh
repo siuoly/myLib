605a9e1fc5299219a88cd07a8be87bef8af2dd93
siuol <louislee123@gmail.com>
2023-10-20 19:13:36 +0800

    exit syntax

--- shell/syntax.sh
+++ shell/syntax.sh
@@ -5,3 +5,10 @@ y=2
  [[ $x -ne $y ]] || [ $x -ne $y ]
  [[ $x -gt $y ]] || [ $x -gt $y ]
  [[ $x -lt $y ]] || [ $x -lt $y ]
+
+ # detect if exist variable
+if [ -z $TMUX ];then  # True, not exist the varaible
+  echo "not exist \$TMUX"
+else
+  echoo "exist \$TMUX"
+fi
