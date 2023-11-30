5172fc648ac5481eba8968937429254648fbd8d3
siuoly-x260 <louislee123@gmail.com>
2023-11-30 16:27:17 +0800

    awk ,shell process usage

--- shell/command.sh
+++ shell/command.sh
@@ -9,3 +9,36 @@ find . -type f -size +2M  # larger than 2 Megabytes.
 unzip "*.zip" -d dir  # unzip all file to <dir>, quote is need for *
 unzip -O big5 file.zip  # unzip traditional chinese archive 繁體中文解壓縮
 
+
+# cut usage
+echo "aaa bbb ccc" |cut -d' ' -f 2 # use ' ' separator instead of tab, select 2nd col ('bbb')
+
+# awk usage
+"                    file: information.txt:
+fristName       lastName        age     city       ID
+
+Thomas          Shelby          30      Rio        400
+Omega           Night           45      Ontario    600
+Wood            Tinker          54      Lisbon     N/A
+Giorgos         Georgiou        35      London     300
+Timmy           Turner          32      Berlin     N/A
+"
+awk '{print $0}' information.txt # full txt
+awk '{print NR,$0}' information.txt # with line number
+awk '{print $NF}' information.txt  # last only
+awk '{print $1, $4}' information.txt # 1,4 columns
+
+awk '/^O/' information.txt  # show line start with 'O'
+awk '/0$/' information.txt  # line end in a '0'
+awk '! /0$/' information.txt  # line not end in a '0'
+
+awk ' /io/{print $0}' information.txt  # line contain 'io'
+awk '/io/{print $1, $2}' information.txt # above see only 1,2 columns
+
+awk -F"\t" '{print $1, $2}' information.txt # set separator
+awk '$3 < 40 { print $0 }' information.txt # selection 3th column < 40
+
+who | awk 'BEGIN {print "Active Sessions"} {print $1,$4}'
+
+awk 'BEGIN { print sqrt(625)}'
+awk 'BEGIN {print atan2(0, -1)}'
--- shell/syntax.sh
+++ shell/syntax.sh
@@ -13,3 +13,10 @@ if [ -z $TMUX ];then  # True, not exist the varaible
 else
   echoo "exist \$TMUX"
 fi
+
+echo $$ # current process PID
+echo $BASHPID # current process/function PID
+ehco $! # previous running process $PID
+
+wait # wair for all process compelete
+waid $PID1 $PID2 # wait for PID1 PID2 process compelete
