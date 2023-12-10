b5e63bc90a3894cc5f124d567f3906e7977ed1c3
siuoly-x260 <louislee123@gmail.com>
2023-12-10 16:49:44 +0800

    python librosa example

---
+++ py/sound/librosa_usage.py
@@ -0,0 +1,20 @@
+import librosa as ra
+ra.ex("choice") # download sound file ,return path
+# ra.util.list_examples() 
+wav = ra.load( ra.ex("choice") )
+
+"""
+brahms     
+choice     
+fishin     
+humpback   
+libri1     
+libri2     
+libri3     
+nutcracker 
+pistachio  
+robin      
+sweetwaltz 
+trumpet    
+vibeace    
+"""
