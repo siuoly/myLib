9e037b45b59e1983a6ef5ccea9a86bcd5f4ce9eb
siuoly-x260 <louislee123@gmail.com>
2023-12-10 16:50:35 +0800

    shell redirect stderr output, shell "wait" usage,example

--- shell/syntax.sh
+++ shell/syntax.sh
@@ -1,22 +1,30 @@
 # Linux shell language syntax command
 # comparsing number
-x=1
-y=2
+x=1; y=2
  [[ $x -eq $y ]] || [ $x -eq $y ]
  [[ $x -ne $y ]] || [ $x -ne $y ]
  [[ $x -gt $y ]] || [ $x -gt $y ]
  [[ $x -lt $y ]] || [ $x -lt $y ]
 
- # detect if exist variable
-if [ -z $TMUX ];then  # True, not exist the varaible
-  echo "not exist \$TMUX"
-else
-  echoo "exist \$TMUX"
-fi
+# detect if exist variable # if True, the varaible not exist 
+[ -z $TMUX ] && echo "not exist \$TMUX" || echo "exist \$TMUX"
 
+# redirect stdout, stderr
+ls >file # equal to: ls 1>file
+ls >file 2>error_log # stderr
+ls >file 2>&1 # both stdout,stderr to file
+ls >file 2>&1 |less # both stdout,stderr to file and pipe
+
+# process id
 echo $$ # current process PID
 echo $BASHPID # current process/function PID
 ehco $! # previous running process $PID
 
+# wait usage
 wait # wair for all process compelete
-waid $PID1 $PID2 # wait for PID1 PID2 process compelete
+wait $PID1 $PID2 # wait for PID1 PID2 process compelete
+## e.g. 
+kill_sleep(){ [ $sleep_pid -ne 0 ] && kill $sleep_pid >/dev/null 2>&1 }
+trap "kill_sleep" USR1 # when receive signal USR1 , by kill -s USR1, # call function kill_sleep
+sleep 1000& PID=$! # make sleep program background, and record PID
+wait # wait sleep process # when receive USR signal, kill sleep immediately
