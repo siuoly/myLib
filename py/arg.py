53bdf233894f7ffc928c3821ed0a9d80a580dab6
siuoly-x260 <louislee123@gmail.com>
2023-12-19 06:44:58 +0800

    argparser example

--- py/arg.py
+++ py/arg.py
@@ -1,10 +1,11 @@
 from argparse import ArgumentParser
+import sys
 
 #============
 parser = ArgumentParser()
-parser.add_argument("postional_arg1", help="identify argument by position")
+parser.add_argument("postional_arg1", help="positional argumant")
 parser.add_argument("postional_arg2", help="identify argument by position")
-parser.add_argument("--key_arg1", help="identify argument by keyword")
+parser.add_argument("--key_arg1", help="keywork argument")
 parser.add_argument("--key_arg2", default="default_value_of key_arg2",
                     help="identify argument by keyword, if not identicate, value is None")
 arg = parser.parse_args(["aaa",
@@ -24,3 +25,19 @@ arg = parser.parse_args(["--ip","127.0.0.1", "--port","7777"])
 print( "indicate", arg )
 arg = parser.parse_args([])
 print( "default", arg )
+
+#=========== argument
+def get_argument(foo=None):
+    p = ArgumentParser()
+    # bool
+    p.add_argument("--mixup",action="store_true",default=False)
+    # int
+    p.add_argument("--batch_size",type=int,default=64,)
+    # float
+    p.add_argument("--lr",type=float,default=1e-3,)
+    # string
+    p.add_argument("--backend",type=str,default="cos",choices=["softmax","cos","kmean"])
+    # string choice
+    p.add_argument("--log_dir",type=str, default="/runs/LR_{lr}__S_{s}__M_{m}")
+    return p.parse_args(foo)
+n = get_argument(sys.argv)
