d052ebca6c6b03a01849c10473d73b747502884e
x260-siuoly <louis@gmial.com>
2024-05-10 01:14:24 +0800

    tensorflow v1 compatibility

--- py/tensorflow/skill.py
+++ py/tensorflow/skill.py
@@ -1,8 +1,13 @@
 import tensorflow as tf
+
+################# tensroflow version1 v1 相容性   ###############
+import tensorflow.compat.v1 as tf
+
+tf.disable_v2_behavior()
+
 ################# pdb ###############
 model = tf.keras.layers.dense(10)
 model.compile()
-model.run_eagerly = True # use this !! after compile() , then can using pdb
+model.run_eagerly = True  # use this !! after compile() , then can using pdb
 
 ####################################
-
