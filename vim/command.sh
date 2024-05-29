fe2f49f0ef22a443ad57d0c2aedd71ac6c8a4fe0
x260-siuoly <louis@gmial.com>
2024-05-29 12:44:42 +0800

    vim usage

---
+++ vim/command.sh
@@ -0,0 +1,17 @@
+# filter command output
+:filter/toggle/map # search command map output
+:filter/\cluasnip/scriptnames # \c:search ignorecase "luasnip" in scriptnames output
+
+# get command output to filter
+redir @a # dump output to register "a"
+silent! set all
+redir END 
+
+redir =>a # dump to variable a
+
+# get command output to filter
+put=execute('nmap')
+<c-r> =put("nmap") # insert mode
+
+# compute Hexadecimal to decimal number 16進位
+<c-r> =0x0539 # get 1337
